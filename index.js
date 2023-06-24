660156cacffa5e139e18f77690a522700d55b766
Ahmad Rana <rana456ahmad789@gmail.com>
2023-06-24 11:36:22 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// function addition(a, b) {
+// 	return a + b;
+// }
+
+// var result = addition(37873.98089, 56);
+// console.log(result);
+
+// function degreetofahrenheit(a) {
+// 	return a * 32 * (9 / 5);
+// }
+// var result = degreetofahrenheit(49);
+// console.log(result);
+// var a = 42;
+// if (a > 10) {
+// 	console.log('you won');
+// } else if (a < 10) {
+// 	console.log('yu lost');
+// } else {
+// 	console.log('try again');
+// }
+
+let name = 'ali';
+if (name === 'ali') {
+	console.log('correct name');
+} else {
+	console.log('you are ahmad not ali');
+}
+
+// function displayGreetings() {
+// 	var name = document.getElementById('nameInput').value;
+// 	console.log('hello! my dear' + '  ' + name + ' ' + 'have a nice day!');
+// }
+// var name = prompt('enter name');
+// console.log('hello!' + ' ' + name);
